e2229e0ae42c98b818405166e7120446b7557a86
MilenaBarre <mbarreto446@gmail.com>
2020-10-31 14:18:18 -0300

    comit2

--- automovel.py
+++ automovel.py
@@ -1,3 +1,15 @@
 class Automovel():
-    def __init__(self,placa):
+    def __init__(self,placa,cor,nportas,tipo_combustivel,quilometragem,renavam,chassi,valor_locacao):
         self._placa = placa
+        self._cor = cor
+        self._npoetas = nportas
+        self._tipo_combustivel = tipo_combustivel
+        self._quilometragem = quilometragem
+        self._renavam = renavam
+        self._chassi = chassi
+        self._valor_locacao = valor_locacao
+    def conAuto(self):
+        print()       
+
+
+
